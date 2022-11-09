13914faac299a627bac351477fc91c74b7276355
Hael04 <hannahael14@gmail.com>
2022-11-08 18:19:17 -0600

    correccion

---
+++ Tarea4/Calculopi.java
@@ -0,0 +1,27 @@
+package parcial1.tarea4;
+
+import java.util.Scanner;
+
+public class Calculopi {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Cuantos términos de presision desea: ");
+            int n = sc.nextInt(); 
+
+            System.out.println("pi = " + pi(n));
+        }
+    }
+
+    public static double pi(int n){
+        int d = 1, sig = 1;
+        double t, s = 0;
+
+        for(int i = 1; i <= n; i++){
+            t = (double) 1 / d * sig;
+            s += t;
+            d += 2;
+            sig *=  -1;
+        }
+        return s *  4;
+    }
+}
